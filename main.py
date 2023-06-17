4888a6fbc94db7f195a588bf2efebc458ef275cd
Ahmad Thoriq Najahi <najahi@zephyrus.id>
2023-06-17 12:43:30 +0700

    feat(main.py): add support for claiming daily reward with optional flag
    Signed-off-by: Ahmad Thoriq Najahi <najahi@zephyrus.id>

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ parser.add_argument("-o",
                     "--output",
                     default="output/stats.html",
                     type=pathlib.Path)
-parser.add_argument("-c", "--cookies", default=None)
-parser.add_argument("-i",
-                    "--info",
+parser.add_argument("-C", "--cookies", default=None)
+parser.add_argument("-c",
+                    "--claim",
+                    default=False,
+                    action=argparse.BooleanOptionalAction)
+parser.add_argument("-V",
+                    "--verbose",
                     default=False,
                     action=argparse.BooleanOptionalAction)
 parser.add_argument("-l",
@@ -33,21 +37,35 @@ parser.add_argument("-l",
 async def main():
     args = parser.parse_args()
     cookies = args.cookies or os.environ["COOKIES"]
-    info = args.info
+    debug = args.verbose
 
-    client = genshin.Client(cookies, debug=info, game=genshin.Game.GENSHIN)
+    client = genshin.Client(cookies, debug=debug, game=genshin.Game.GENSHIN)
     user = await client.get_full_genshin_user(0, lang=args.lang)
 
     abyss = user.abyss.current if user.abyss.current.floors else user.abyss.previous
 
     diary = await client.get_diary()
 
+    if args.claim:
+        try:
+            await client.claim_daily_reward(lang=args.lang, reward=False)
+        except genshin.AlreadyClaimed:
+            pass
+        finally:
+            reward = await client.claimed_rewards(lang=args.lang).next()
+            reward_info = await client.get_reward_info()
+    else:
+        reward = None
+        reward_info = None
+
     template = jinja2.Template(args.template.read_text())
     rendered = template.render(
         user=user,
         lang=args.lang,
         abyss=abyss,
+        reward=reward,
         diary=diary,
+        reward_info=reward_info,
     )
     args.output.write_text(rendered)
 
